9587e29383c1b1e67aa3a69090362d6485cdba73
kingstar1115 <hatoyama1115@gmail.com>
2023-03-13 23:24:00 +0900

    Layout Example First my Proud

--- lib/main.dart
+++ lib/main.dart
@@ -5,228 +5,111 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: ListTileSelectExample(),
-    );
-  }
-}
+    Color color = Theme.of(context).primaryColor;
 
-class ListTileSelectExample extends StatefulWidget {
-  const ListTileSelectExample({super.key});
-
-  @override
-  ListTileSelectExampleState createState() => ListTileSelectExampleState();
-}
-
-class ListTileSelectExampleState extends State<ListTileSelectExample> {
-  bool isSelectionMode = false;
-  final int listLength = 30;
-  late List<bool> _selected;
-  bool _selectAll = false;
-  bool _isGridMode = false;
-
-  @override
-  void initState() {
-    super.initState();
-    initializeSelection();
-  }
-
-  void initializeSelection() {
-    _selected = List<bool>.generate(listLength, (_) => false);
-  }
-
-  @override
-  void dispose() {
-    _selected.clear();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'ListTile selection',
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
           ),
-          leading: isSelectionMode
-              ? IconButton(
-                  icon: const Icon(Icons.close),
-                  onPressed: () {
-                    setState(() {
-                      isSelectionMode = false;
-                    });
-                    initializeSelection();
-                  },
-                )
-              : const SizedBox(),
-          actions: <Widget>[
-            if (_isGridMode)
-              IconButton(
-                icon: const Icon(Icons.grid_on),
-                onPressed: () {
-                  setState(() {
-                    _isGridMode = false;
-                  });
-                },
-              )
-            else
-              IconButton(
-                icon: const Icon(Icons.list),
-                onPressed: () {
-                  setState(() {
-                    _isGridMode = true;
-                  });
-                },
-              ),
-            if (isSelectionMode)
-              TextButton(
-                  child: !_selectAll
-                      ? const Text(
-                          'select all',
-                          style: TextStyle(color: Colors.white),
-                        )
-                      : const Text(
-                          'unselect all',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                  onPressed: () {
-                    _selectAll = !_selectAll;
-                    setState(() {
-                      _selected =
-                          List<bool>.generate(listLength, (_) => _selectAll);
-                    });
-                  }),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschienen lies at the foot of the Bluemlisalp in the Bernese'
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a'
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activites '
+        'enjoyed here includerowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/img(14).jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
-        body: _isGridMode
-            ? GridBuilder(
-                isSelectionMode: isSelectionMode,
-                selectedList: _selected,
-                onSelectionChange: (bool x) {
-                  setState(() {
-                    isSelectionMode = x;
-                  });
-                },
-              )
-            : ListBuilder(
-                isSelectionMode: isSelectionMode,
-                selectedList: _selected,
-                onSelectionChange: (bool x) {
-                  setState(() {
-                    isSelectionMode = x;
-                  });
-                },
-              ));
-  }
-}
-
-class GridBuilder extends StatefulWidget {
-  const GridBuilder({
-    super.key,
-    required this.selectedList,
-    required this.isSelectionMode,
-    required this.onSelectionChange,
-  });
-
-  final bool isSelectionMode;
-  final Function(bool)? onSelectionChange;
-  final List<bool> selectedList;
-
-  @override
-  GridBuilderState createState() => GridBuilderState();
-}
-
-class GridBuilderState extends State<GridBuilder> {
-  void _toggle(int index) {
-    if (widget.isSelectionMode) {
-      setState(() {
-        widget.selectedList[index] = !widget.selectedList[index];
-      });
-    }
+      ),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-        itemCount: widget.selectedList.length,
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (_, int index) {
-          return InkWell(
-            onTap: () => _toggle(index),
-            onLongPress: () {
-              if (!widget.isSelectionMode) {
-                setState(() {
-                  widget.selectedList[index] = true;
-                });
-                widget.onSelectionChange!(true);
-              }
-            },
-            child: GridTile(
-                child: Container(
-              child: widget.isSelectionMode
-                  ? Checkbox(
-                      onChanged: (bool? x) => _toggle(index),
-                      value: widget.selectedList[index])
-                  : const Icon(Icons.image),
-            )),
-          );
-        });
-  }
-}
-
-class ListBuilder extends StatefulWidget {
-  const ListBuilder({
-    super.key,
-    required this.selectedList,
-    required this.isSelectionMode,
-    required this.onSelectionChange,
-  });
-
-  final bool isSelectionMode;
-  final List<bool> selectedList;
-  final Function(bool)? onSelectionChange;
-
-  @override
-  State<ListBuilder> createState() => _ListBuilderState();
-}
-
-class _ListBuilderState extends State<ListBuilder> {
-  void _toggle(int index) {
-    if (widget.isSelectionMode) {
-      setState(() {
-        widget.selectedList[index] = !widget.selectedList[index];
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: widget.selectedList.length,
-        itemBuilder: (_, int index) {
-          return ListTile(
-              onTap: () => _toggle(index),
-              onLongPress: () {
-                if (!widget.isSelectionMode) {
-                  setState(() {
-                    widget.selectedList[index] = true;
-                  });
-                  widget.onSelectionChange!(true);
-                }
-              },
-              trailing: widget.isSelectionMode
-                  ? Checkbox(
-                      value: widget.selectedList[index],
-                      onChanged: (bool? x) => _toggle(index),
-                    )
-                  : const SizedBox.shrink(),
-              title: Text('item $index'));
-        });
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
