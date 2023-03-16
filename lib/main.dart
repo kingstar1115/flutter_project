2eceabf6245c9f2c31cccfb9e4f7a830d4ee6a16
kingstar1115 <hatoyama1115@gmail.com>
2023-03-16 19:25:03 +0900

    Adding interactivity B

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'src/app_bar_example.dart';
 import 'src/tabbox_a.dart';
+import 'src/tapbox_b.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AppBarExample(),
+      home: TapboxBExample(),
     );
   }
 }
---
+++ lib/src/tapbox_b.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({super.key});
+
+  @override
+  State<ParentWidget> createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        child: TapboxB(
+      active: _active,
+      onChanged: _handleTapboxChanged,
+    ));
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  const TapboxB({
+    super.key,
+    this.active = false,
+    required this.onChanged,
+  });
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: const TextStyle(
+              fontSize: 32.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TapboxBExample extends StatelessWidget {
+  const TapboxBExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Demo'),
+          ),
+          body: const Center(
+            child: ParentWidget(),
+          )),
+    );
+  }
+}
